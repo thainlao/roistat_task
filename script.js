1f33b86a9c4c9c29139ab83e1bcc82353a3c37f4
thainlao <121868297+thainlao@users.noreply.github.com>
2024-10-10 16:52:50 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -10,3 +10,22 @@ form.addEventListener('submit', function (e) {
 goToLanding.addEventListener('click', function () {
     window.location.href = 'landing.html';
 });
+
+function toggleMenu() {
+    const menu = document.getElementById("dropdownMenu");
+    menu.style.display = (menu.style.display === "block") ? "none" : "block";
+}
+
+function closeMenu() {
+    const menu = document.getElementById("dropdownMenu");
+    menu.style.display = "none";
+}
+
+window.onclick = function(event) {
+    const menu = document.getElementById("dropdownMenu");
+    if (event.target !== menu && event.target.closest('.header_menu') !== menu) {
+        closeMenu();
+    }
+}
+
+document.getElementById("closemenu").addEventListener("click", closeMenu);
